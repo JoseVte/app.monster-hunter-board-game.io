edeb4fc9b82f0072b38fc71715c62a7cffeacf5f
Jose Vicente Orts Romero <jvortsromero@gmail.com>
2023-05-26 14:53:23 +0200

    fix tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,22 @@
 
 namespace Tests;
 
+use Database\Seeders\RolesSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $uses = array_flip(class_uses_recursive(static::class));
+
+        if (isset($uses[RefreshDatabase::class])) {
+            $this->artisan('db:seed', ['--class' => RolesSeeder::class]);
+        }
+    }
 }
